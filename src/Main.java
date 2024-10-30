35a9c82952d07317745455fc84c366cf59bf2809
jermzy427 <waicjc2027@student.cis.edu.hk>
2024-10-30 09:42:39 +0800

    Quiz 5 Done

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class Main {
 
     /**
@@ -8,8 +9,17 @@ public class Main {
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
+        int[] nums  = new int[array1.length+ array2.length];
+        int position = 0;
+        for (int i = 0; i<array1.length; i++){
+            nums[i] = array1[i];
+            position++;
+        }
+        for (int j = 0; j< array2.length;j++){
+            nums[position] = array2[j];
+            position++;
+        }
+        return nums;
 
     }
 
@@ -21,8 +31,22 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
+        int tempnum = 0;
+        int a1position = 0;
+        int a2position = 1;
 
-        return null;
+        int[] nums = new int[array1.length+array2.length];
+
+        for(int i = 0; i<array1.length; i++){
+            nums[a1position] = array1[i];
+            a1position += 2;
+        }
+        for(int j = 0; j<array2.length; j++){
+            nums[a2position] = array2[j];
+            a2position += 2;
+        }
+        System.out.print(Arrays.toString(nums));
+        return nums;
 
     }
 
@@ -34,8 +58,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
+        int[] nums = new int[array1.length];
+        for(int i = 0; i<array1.length; i++){
+            nums[i] = array1[i]*array2[i];
+        }
+        return nums;
 
     }
 
@@ -56,8 +83,21 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
+        int ascii = 0;
+        int numcaptitals = 0;
+        int[] capitals = new int[words.length];
+        for (int i = 0; i< words.length;i++){
+            for (int j = 0; j < words[i].length(); j++){
+                ascii = (int)words[i].charAt(j);
+                if(ascii>=65 && ascii<=90){
+                    numcaptitals++;
+                }
+            }
+            capitals[i] = numcaptitals;
+            numcaptitals = 0;
+        }
+        System.out.println(Arrays.toString(capitals));
+        return capitals;
 
     }
 
